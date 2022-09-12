91040f274fbcfc4f3093174b17a97c2d936f2c60
tim diender <tim.diender@student.fontys.nl>
2022-09-12 11:05:28 +0200

    add class libray

---
+++ circustrein/Classen/Class.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Classen
+{
+    public static class Class
+    {
+        public static bool StartsWithUpper(this string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return false;
+
+            char ch = str[0];
+            return char.IsUpper(ch);
+        }
+    }
+}
--- circustrein/circustrein/Program.cs
+++ circustrein/circustrein/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Classen;
 
 namespace circustrein
 {
@@ -6,7 +7,35 @@ namespace circustrein
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int row = 0;
+
+            do
+            {
+                if (row == 0 || row >= 25)
+                    ResetConsole();
+
+                string? input = Console.ReadLine();
+                if (string.IsNullOrEmpty(input)) break;
+                Console.WriteLine($"Input: {input}");
+                Console.WriteLine("Begins with uppercase? " +
+                     $"{(input.StartsWithUpper() ? "Yes" : "No")}");
+                Console.WriteLine();
+                row += 4;
+            } while (true);
+            return;
+
+            // Declare a ResetConsole local method
+            void ResetConsole()
+            {
+                if (row > 0)
+                {
+                    Console.WriteLine("Press any key to continue...");
+                    Console.ReadKey();
+                }
+                Console.Clear();
+                Console.WriteLine($"{Environment.NewLine}Press <Enter> only to exit; otherwise, enter a string and press <Enter>:{Environment.NewLine}");
+                row = 3;
+            }
         }
     }
 }
