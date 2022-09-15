6d95452f501c7b061456552fb4cf71a181f0487c
tim diender <tim.diender@student.fontys.nl>
2022-09-15 10:53:14 +0200

    poging unit test

---
+++ circustrein/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Classen;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TryaddanimalTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
